fc26bac6b0a021bbb955302aac62bda3fabf7ada
Thales Domingues <thdomingues.dev@gmail.com>
2021-05-26 22:21:10 -0400

    feat: custom global theme created

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,39 @@ import authProvider from './services/authProvider';
 
 import UserList from './components/UserList';
 
+import { createMuiTheme } from '@material-ui/core/styles';
+import VideoLibrary from '@material-ui/icons/VideoLibrary';
+import PeopleAlt from '@material-ui/icons/PeopleAlt';
+
 const dataProvider = jsonServerProvider('http://localhost:5000');
+
+const theme = createMuiTheme({
+  overrides: {
+    MuiTableCell: {
+      sizeSmall: {
+        padding: '16px 6px',
+      }
+    },
+    MuiAppBar: {
+      colorSecondary: {
+        backgroundColor: '#666'
+      }
+    }
+  },
+});
 
 function App() {
 
   return (
-    <Admin dataProvider={dataProvider} authProvider={authProvider}>
+    <Admin theme={theme} dataProvider={dataProvider} authProvider={authProvider}>
       <Resource
         name="posts"
         list={PostList}
         create={PostCreate}
         edit={PostEdit}
+        icon={VideoLibrary}
       />
-      <Resource name="users" list={UserList}/>
+      <Resource name="users" list={UserList} icon={PeopleAlt}/>
     </Admin>
   );
 }
