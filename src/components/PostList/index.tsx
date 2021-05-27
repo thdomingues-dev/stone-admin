d8e318e3d02370fb8ee331fd2b5896b2362ad783
Thales Domingues <thdomingues.dev@gmail.com>
2021-05-26 22:20:08 -0400

    feat: aside component added

--- src/components/PostList/index.tsx
+++ src/components/PostList/index.tsx
@@ -4,19 +4,35 @@ import {
   TextField,
   DateField,
   EditButton,
-  DeleteButton
+  useListContext,
+  ChipField
 } from 'react-admin';
+
+import { Typography } from '@material-ui/core';
+
+const Aside = () => {
+  const { data, ids} = useListContext();
+  return (
+      <div style={{ width: 200, margin: '1em' }}>
+          <Typography variant="h6">Status Publicação</Typography>
+          <Typography variant="body2">
+            Total views: {ids.map(id => data[id]).reduce((sum, post) => sum + post.views, 0)}
+          </Typography>
+      </div>
+  );
+};
 
 function PostList(props: any) {
   return (
-    <List title="Listar Posts" {...props}>
+    <List title="Listar Posts" aside={<Aside />} {...props}>
       <Datagrid>
-        <TextField source="id" label="Identificacao" />
-        <TextField source="title" label="Titulo" />
-        <TextField source="body" label="Descricao" />
-        <DateField source="publishedAt" label="Publicacao" locales="pt-BR" />
+        <TextField source="id" label="ID" />
+        <TextField source="title" label="TITULO" />
+        <TextField source="body" label="DESCRICAO" />
+        <DateField source="publishedAt" label="PUBLICACAO" locales="pt-BR" />
+        <TextField source="views" label="VISUALIZACOES  " />
+        <ChipField source="category" label="TIPO" />
         <EditButton basePath="/posts" />
-        <DeleteButton basePath="/posts" />
       </Datagrid>
     </List>
   );
